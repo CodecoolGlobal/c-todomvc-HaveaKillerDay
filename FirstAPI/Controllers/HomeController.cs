237b098637b5bcd50cc52c94d9e7dc69ba262cd4
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-09-29 19:44:23 +0200

    API works so far

---
+++ FirstAPI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void AddToDo()
+        {
+
+        }
+    }
+}
